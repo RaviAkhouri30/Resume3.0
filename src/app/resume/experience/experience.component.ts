8678c419923d4cfef50945b333f612e1fb8aeac8
Ravi Akhouri <ravi.akhouri@gmail.com>
2022-11-27 01:58:45 +0530

    read more and read less implemented

--- src/app/resume/experience/experience.component.ts
+++ src/app/resume/experience/experience.component.ts
@@ -10,6 +10,8 @@ import { ExperienceService } from './service/experience.service';
 export class ExperienceComponent implements OnInit {
 
   private workExpData: ExperienceModel[];
+  private expandedUid: string = ''
+  private prevExpandedUid: string = ''
 
   constructor(
     private experienceService: ExperienceService
@@ -22,7 +24,7 @@ export class ExperienceComponent implements OnInit {
   }
 
   public getData = (): void => {
-    this.experienceService.getData().subscribe((res:ExperienceModel[]) => {
+    this.experienceService.getData().subscribe((res: ExperienceModel[]) => {
       this.workExpData = [...res];
       console.log(this.workExpData);
     });
@@ -33,16 +35,37 @@ export class ExperienceComponent implements OnInit {
   }
 
   public onReadMore = (uid: string): void => {
-    if(document.getElementById(uid)){
+    this.onReadLess(uid);
+    this.expandedUid = uid;
+    if (document.getElementById(uid)) {
       const element = document.getElementById(uid) as HTMLDivElement;
       const _element = document.getElementById('cont' + uid) as HTMLDivElement;
-      _element.style[<any>'--max-height'] = '100%';
-      _element.style.overflow = 'visible';
+      _element.style[<any>'overflow-y'] = 'scroll';
+      _element.style[<any>'height'] = '75%';
       const _elementC = document.getElementById('_cont' + uid) as HTMLDivElement;
       _elementC.style.height = '100%';
-      _elementC.style.overflow = 'visible';
-      element.style.height = '200px';
+      element.style.height = '300px';
     }
+  }
+
+  public onReadLess = (uid: string): void => {
+    this.workExpData.forEach(e => {
+      if(e.getUid() !== uid){
+        if (document.getElementsByClassName(e.getUid())) {
+          const element = document.getElementById(e.getUid()) as HTMLDivElement;
+          const _element = document.getElementById('cont' + e.getUid()) as HTMLDivElement;
+          _element.style[<any>'overflow-y'] = 'hidden';
+          _element.style[<any>'height'] = '100%';
+          const _elementC = document.getElementById('_cont' + e.getUid()) as HTMLDivElement;
+          _elementC.style.height = '100%';
+          element.style.height = '130px';
+        }
+      }
+    });
+  }
+
+  public isExpanded = (): string => {
+    return this.expandedUid;
   }
 
 }
--- src/app/resume/experience/service/work-exp-data.ts
+++ src/app/resume/experience/service/work-exp-data.ts
@@ -2,7 +2,7 @@ export const workExpData = [
     {
         uid: 'work-exp-1',
         companyName: 'Mindtree Ltd',
-        tenure: 'August 2019 - present',
+        tenure: 'August 2021 - present',
         desc: `My Role was Angular Developer and responsibilty was to integrate rest
         api's and to implement Bussiness logics in client side application. We
         were given a task to migrate the old Application which was based on the
@@ -12,5 +12,40 @@ export const workExpData = [
         high charts and typescript features to accomplish my job. I have been
         awarded 4 times in row for my exeptional contribution towards the
         Project.`
+    },
+    {
+        uid: 'work-exp-2',
+        companyName: 'Wipro Ltd',
+        tenure: 'October 2019 - August 2021',
+        desc: `As an Angular Developer, my responsibility is to implement Business Logic, 
+        Validations and integration of RestApis. 
+        Also Taken a responsibility as a Module Lead to make a coordination in a Team for smooth approach
+         towards common goal of completing Module/Functionalities and UAT/SIT Releases, 
+         and also coordinate with Release Team for Deployment on SIT/UAT. 
+         I started as a Prototype Developer and soon moved to development and 
+         I worked on modules such as PVU, Budget, EDP and now handling DDO Modules. 
+         I also achieved an award for Outperformance in Q1. In PVU, I worked on a sub Module.i.e. ROP, after completing
+          my tasks of development of screens for the same, moved to Budget, 
+          create a number of common, reusable and efficient code like common components and directives for faster and more 
+          efficient way of development to reduce the development time and complexity, then moved to EDP and given 
+          the task to complete workflow integration and resolving bugs towards UAT sign off and then 
+          given a responsibility of Module Lead in DDO Module to make a coordination and to contribute towards the completion of Module 
+          and subsequently approaching towards SIT and then UAT Release. 
+          To handle similar looking screens but with different functionality and behaviour using configuration layer, 
+          created each section as separate components and also a common configuration component and container 
+          that handles all the different child components. 
+          The Configuration was created to handle the behaviour of different screens such as default fields values, 
+          remove some fields and add some fields, remove section and add section, changing the contents of DOM. 
+          A Configuration service layer is also created to dynamically change the configuration to handle behaviour 
+          of view and edit case 
+          and fields validation and access. Also, Implemented oops such as encapsulation, 
+          abstraction and inheritance and also reduce the code redundancy to a large extent. 
+          Implemented a robust validation for both save as draft and submit case. 
+          Properly modelling of data and created a complete module structure from scratch. 
+          Due to the implementation of common code Structure, the development time is reduced and making use of oops, 
+          created a structure that enables reducing of errors/bugs and easy to debug. Using debugging techniques, 
+          resolve the SIT bugs to enable faster releases and making the code bugs/errors free. Also implemented the 
+          State management system in the service layer using rxjs Behavioural Subject. 
+        I also take Technical Rounds as a part of Angular Recruitments.`
     }
 ];
