d1d4b000e95d6893a4fdd48281b59e19bb706af6
Ravi Akhouri <ravi.akhouri@gmail.com>
2024-12-26 17:49:36 +0530

    migration(ng): ng update completed

--- src/app/resume/about-me/about-me.component.ts
+++ src/app/resume/about-me/about-me.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  email = "ravi.akhouri@gmail.com"
   constructor() { }
 
   ngOnInit(): void {
